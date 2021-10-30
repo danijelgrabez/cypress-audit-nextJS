5a74df054e0a6578611f53b319d28eaebe4d83c6
Danijel Grabež <danijel.grabez@sesamecare.com>
2021-10-30 15:10:19 +0200

    :two: Configure lighthouse reports and update tests

--- cypress/integration/audit.spec.js
+++ cypress/integration/audit.spec.js
@@ -1,12 +1,9 @@
 /// <reference types="cypress" />
 
 context('Accessibility and Performance Testing', () => {
-  beforeEach(() => {
+  it.only('audits the home page', () => {
     cy.visit('/');
-  });
 
-  it('audits the home page', () => {
-    // Note: Bare config
     cy.lighthouse({
       accessibility: 40,
       'best-practices': 50,
@@ -14,14 +11,67 @@ context('Accessibility and Performance Testing', () => {
       pwa: 20,
       performance: 49,
     });
-  });
-  });
 
-  it('pa11y', () => {
+    // NOTE: Available options
+    // https://github.com/mfrachet/cypress-audit/blob/5971d705670147b9dabb2ab0c8981a734d25b29d/index.d.ts#L6
     cy.pa11y({
       headers: {
         title: true,
       },
+    });
+  });
+
+  // NOTE: Custom thresholds
+  // Read more about recommended config for better results:
+  // https://github.com/mfrachet/cypress-audit/blob/master/docs/lighthouse.md#lighthouse-scores-may-be-different-between-local-run-and-cypress-audit
+  it('audits the homepage with custom threshold (will produce a failing test)', () => {
+    cy.visit('/');
+
+    const customThresholds = {
+      accessibility: 100, // NOTE: test will fail
+      performance: 90,
+    };
+
+    const desktopConfig = {
+      formFactor: 'desktop',
+      screenEmulation: {
+        width: 1350,
+        height: 940,
+        deviceScaleRatio: 1,
+        mobile: false,
+        disable: false,
+      },
+      throttling: {
+        rttMs: 40,
+        throughputKbps: 11024,
+        cpuSlowdownMultiplier: 1,
+        requestLatencyMs: 0,
+        downloadThroughputKbps: 0,
+        uploadThroughputKbps: 0,
+      },
+    };
+
+    cy.lighthouse(customThresholds, desktopConfig);
+  });
+
+  const posts = ['ssg-ssr', 'pre-rendering'];
+  posts.forEach((post) => {
+    it(`audits the ${post} post`, () => {
+      cy.visit(`/posts/${post}`);
+
+      cy.lighthouse({
+        accessibility: 40,
+        'best-practices': 50,
+        seo: 50,
+        pwa: 20,
+        performance: 49,
+      });
+
+      cy.pa11y({
+        headers: {
+          title: true,
+        },
+      });
     });
   });
 });
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -16,6 +16,10 @@
  * @type {Cypress.PluginConfig}
  */
 const { lighthouse, pa11y, prepareAudit } = require('cypress-audit');
+const ReportGenerator = require('lighthouse/report/generator/report-generator');
+const fs = require('fs');
+const path = require('path');
+const { format } = require('date-fns');
 
 module.exports = (on, config) => {
   on('before:browser:launch', (browser = {}, launchOptions) => {
@@ -30,9 +34,37 @@ module.exports = (on, config) => {
    * NOTE: Bare config
    * Reports are exposed in Cypress console
    */
-  on('task', {
-    lighthouse: lighthouse(),
-    pa11y: pa11y(console.log.bind(console)),
-  });
+  // on('task', {
+  //   lighthouse: lighthouse(),
+  //   pa11y: pa11y(console.log.bind(console)),
+  // });
 
+  /**
+   * NOTE: Custom config
+   * Reports are stored in `cypress/reports` folder
+   */
+  on('task', {
+    lighthouse: lighthouse((lighthouseReport) => {
+      // console.log(lighthouseReport); // raw lighthouse reports
+      const dateFormat = format(Date.now(), 'dd.MM.yyyy_HH:mm');
+      const [_, folderPath] = lighthouseReport.lhr.requestedUrl.split('http://localhost:3000/');
+      const reportFileName = `${dateFormat}_lhreport.html`;
+      // Organize reports
+      fs.mkdir(`cypress/reports/${folderPath}`, { recursive: true }, (err) => {
+        if (err) throw err;
+
+        fs.writeFileSync(
+          path.resolve(process.cwd(), `cypress/reports/${folderPath}/${reportFileName}`),
+          ReportGenerator.generateReport(lighthouseReport.lhr, 'html')
+        );
+      });
+    }),
+    /**
+
+     * NOTE: It is possible to create a custom report for pa11y as well
+     */
+    pa11y: pa11y((pa11yReport) => {
+      console.log(pa11yReport); // raw pa11y reports
+    }),
+  });
 };
